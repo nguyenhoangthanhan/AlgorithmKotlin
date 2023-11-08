aec2bacff796e45bb15eb58db8b4dd9749bdd5fb
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-11-08 15:14:32 +0700

    Create mobile test project JokeeSingleServingAPP and create UI for this project

--- ZensAlgorithmsTest/src/test/kotlin/MiniMaxSumTest.kt
+++ ZensAlgorithmsTest/src/test/kotlin/MiniMaxSumTest.kt
@@ -1,7 +1,7 @@
 
 import org.junit.Assert.assertEquals
-import org.junit.runner.RunWith
-import java.util.*
+import org.junit.Test
+
 
 class MiniMaxSumTest {
 
@@ -10,62 +10,62 @@ class MiniMaxSumTest {
     private val inputTestCase2 =
         longArrayOf(10000000000000, 30000000000000, 50000000000000, 70000000000000, 90000000000000)
 
-    @org.junit.Test
+    @Test
     fun findMiniMaxSum_TestCase1() {
         assertEquals("16 24", miniMaxSum.findMiniMaxSum(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun findMiniMaxSum_TestCase2() {
         assertEquals("160000000000000 240000000000000", miniMaxSum.findMiniMaxSum(inputTestCase2))
     }
 
-    @org.junit.Test
+    @Test
     fun sumArray_TestCase1() {
         assertEquals(25, miniMaxSum.sumArray(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun sumArray_TestCase2() {
         assertEquals(250000000000000, miniMaxSum.sumArray(inputTestCase2))
     }
 
-    @org.junit.Test
+    @Test
     fun minOfArray_TestCase1() {
         assertEquals(1, miniMaxSum.minOfArray(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun minOfArray_TestCase2() {
         assertEquals(10000000000000, miniMaxSum.minOfArray(inputTestCase2))
     }
 
-    @org.junit.Test
+    @Test
     fun maxOfArray_TestCase1() {
         assertEquals(9, miniMaxSum.maxOfArray(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun maxOfArray_TestCase2() {
         assertEquals(90000000000000, miniMaxSum.maxOfArray(inputTestCase2))
     }
 
-    @org.junit.Test
+    @Test
     fun evenElementsOfArray_TestCase1() {
         assertEquals(listOf<Long>(), miniMaxSum.evenElementsOfArray(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun evenElementsOfArray_TestCase2() {
         assertEquals(listOf<Long>(10000000000000, 30000000000000, 50000000000000, 70000000000000, 90000000000000), miniMaxSum.evenElementsOfArray(inputTestCase2))
     }
 
-    @org.junit.Test
+    @Test
     fun oddElementsOfArray_TestCase1() {
         assertEquals(listOf<Long>(1,3,5,7,9), miniMaxSum.oddElementsOfArray(inputTestCase1))
     }
 
-    @org.junit.Test
+    @Test
     fun oddElementsOfArray_TestCase2() {
         assertEquals(listOf<Long>(), miniMaxSum.oddElementsOfArray(inputTestCase2))
     }
