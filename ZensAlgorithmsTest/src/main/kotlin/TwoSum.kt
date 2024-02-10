8999d1f1441f47291e87e72d14a16fc787f6c363
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-02-10 17:07:20 +0700

    two sum problem

---
+++ ZensAlgorithmsTest/src/main/kotlin/TwoSum.kt
@@ -0,0 +1,33 @@
+import javax.swing.tree.TreeNode
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class TwoSum {
+    fun isBalanced(root: TreeNode?): Boolean {
+        return getHight(root) != -1
+    }
+
+    fun getHight(root: TreeNode?): Int {
+        if (root == null){
+            return 0
+        }
+        var leftHight = getHight(root.left)
+        var rightHight = getHight(root.right)
+
+        if(leftHight == -1 || rightHight == -1){
+            return -1
+        }
+        if(Math.abs(leftHight - rightHight) > 1){
+            return -1
+        }
+        return Math.max(leftHight, rightHight) + 1
+    }
+}
