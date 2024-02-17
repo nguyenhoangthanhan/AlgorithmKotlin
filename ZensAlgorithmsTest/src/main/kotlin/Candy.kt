9ee46c67431ff6efce577384ee083e16666df89b
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-02-18 00:46:46 +0700

    Coding solution Problem 135. Candy in leetcode

--- ZensAlgorithmsTest/src/main/kotlin/Candy.kt
+++ ZensAlgorithmsTest/src/main/kotlin/Candy.kt
@@ -19,4 +19,29 @@ class Candy {
         }
         return ans
     }
+
+    fun solutionCandy2(ratings: IntArray): Int{
+        if (ratings.size == 1) return 1
+
+        val candy = IntArray(ratings.size)
+        candy[0] = 1
+
+        for (i in 1 until ratings.size) {
+            if (ratings[i] > ratings[i-1]){
+                candy[i] = candy[i - 1] + 1
+            } else {
+                candy[i] = 1
+            }
+        }
+
+        var totalCandy = candy[ratings.size - 1]
+        for (i in ratings.size - 2 downTo 0) {
+            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+                candy[i] = candy[i + 1] + 1
+            }
+            totalCandy += candy[i]
+        }
+
+        return totalCandy
+    }
 }
