f847330656bc40ab8f7e02744cac9b435b8dec02
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-02-28 22:33:19 +0700

    Implemented tree node day 2

--- ZensAlgorithmsTest/src/main/kotlin/TreeNode.kt
+++ ZensAlgorithmsTest/src/main/kotlin/TreeNode.kt
@@ -30,17 +30,17 @@ class Node(
      * If the tree already contains the given value, nothing is performed
      * @param value
      */
-    fun insert(value: Int){
-        if (value > this.key){
-            if (this.right == null){
+    fun insert(value: Int) {
+        if (value > this.key) {
+            if (this.right == null) {
                 this.right = Node(value)
-            }else{
+            } else {
                 this.right?.insert(value)
             }
-        }else if(value < this.key){
-            if (this.left == null){
+        } else if (value < this.key) {
+            if (this.left == null) {
                 this.left = Node(value)
-            }else{
+            } else {
                 this.left?.insert(value)
             }
         }
@@ -50,8 +50,8 @@ class Node(
      * Delete the value from the given tree. If the tree does not contain the value, the tree remains unchanged.
      * @param value
      */
-    fun delete(value: Int){
-        when{
+    fun delete(value: Int) {
+        when {
             value > key -> sc
         }
     }
@@ -62,8 +62,8 @@ class Node(
      * @param node sub-tree that potentially might contain the sought value
      * @param parent node's parent
      */
-    private fun scan(value: Int, node: Node?, parent: Node?){
-        if (node == null){
+    private fun scan(value: Int, node: Node?, parent: Node?) {
+        if (node == null) {
             println("value $value seem not present in the tree.")
             return
         }
@@ -82,15 +82,15 @@ class Node(
      * @param node node that is to be removed
      * @param parent parent of the node to be removed
      */
-    private fun removeNode(node: Node, parent: Node?){
-        node.left?.let{
+    private fun removeNode(node: Node, parent: Node?) {
+        node.left?.let {
             run {
-                node.right?.let{
+                node.right?.let {
                     removeTwoChildNode(node)
                 } ?: removeSingleChildNode(node, leftChild)
             }
-        } ?: run{
-            node.right?.let{rightChild ->
+        } ?: run {
+            node.right?.let { rightChild ->
                 remove
             }
         }
@@ -101,11 +101,11 @@ class Node(
      * @param node
      * @param parent
      */
-    private fun removeNoChildNode(node: Node, parent: Node?){
-        parent?.let{p ->
-            if (node == p.left){
+    private fun removeNoChildNode(node: Node, parent: Node?) {
+        parent?.let { p ->
+            if (node == p.left) {
                 p.left = null
-            } else if (node == p.right){
+            } else if (node == p.right) {
                 p.right = null
             }
         } ?: throw IllegalStateException(
@@ -119,4 +119,47 @@ class Node(
      * The process of elimination is to find the biggest key in the left sub-tree and replace the key
      * of the node is to be deleted with that key
      */
+    private fun removeTwoChildNode(node: Node?) {
+        val leftChild = node?.left!!
+        leftChild.right?.let {
+            val maxParent = findParentOfMaxChild(leftChild)
+            maxParent.right?.let {
+                node.key = it.key
+                maxParent.right = null
+            } ?: throw IllegalStateException("Node with max child must have the right child!")
+        } ?: run {
+            node.key = leftChild.key
+            node.left = leftChild.left
+        }
+    }
+
+    /**
+     * Return a node whose right child contains the biggest value in the given sub-tree
+     * Assume that the node n has a non-null right child.
+     *
+     * @param n
+     */
+    private fun findParentOfMaxChild(n: Node?): Node {
+        return n?.right?.let { r -> r.right?.let { findParentOfMaxChild(r) } ?: n }
+            ?: throw IllegalStateException("Right child must be non-null")
+    }
+
+    /**
+     * Remove a parent that has only one child.
+     * Removal is effectively is just copying the data from the child parent to the parent.
+     * @param parent Node to be deleted. Assume that it has just one child
+     * @param child Assume it is a child of the parent.
+     */
+    private fun removeSingleChildNode(parent: Node, child: Node){
+        parent.key = child.key
+        parent.left = child.left
+        parent.right = child.right
+    }
+
+    fun visit(): Array<Int>{
+        val a = left?.visit() ?: emptyArray()
+        val b = right?.visit() ?: emptyArray()
+        return a + arrayOf(key) + b
+    }
+
 }
