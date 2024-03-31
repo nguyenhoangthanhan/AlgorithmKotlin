7b224e08ca9b1ab46fa60f17fa9ef98486018b0e
NHTA1999 <skyannguyenhoangthanh@gmail.com>
2024-03-31 23:30:57 +0700

    Coding two sum problem follow a innovative way

---
+++ ZensAlgorithmsTest/src/main/kotlin/TwoSumV2.kt
@@ -0,0 +1,19 @@
+class TwoSumV2 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val hashMap = mutableMapOf<Int, Int>() // value to index
+
+        for((i,v) in nums.withIndex()) {
+            val diff = target -v
+            val indexForDiffValue = hashMap.get(diff)
+
+            indexForDiffValue?.let {
+                return@twoSum intArrayOf(indexForDiffValue, i)
+            }
+
+            hashMap[v] = i
+        }
+
+        throw IllegalStateException("no solution found")
+    }
+}
+}
