7567b4e27a8f764ba454133612954fb5f10930de
NHTA1999 <skyannguyenhoangthanh@gmail.com>
2024-04-20 03:02:52 +0700

    Vector demo ver 2

--- ZensAlgorithmsTest/src/main/kotlin/VectorDemo1.kt
+++ ZensAlgorithmsTest/src/main/kotlin/VectorDemo1.kt
@@ -1,8 +1,16 @@
+import java.util.*
+
 class VectorDemo1 {
     fun demo(){
         var planets =  Vector<String>();
         planets.addAll(listOf("Mercury", "Venus", "Earth", "Pluto"))
 
         println("Planets = " + planets)
+
+
+        var planets2 =  Vector<String>();
+        planets2.addAll(listOf("Mercury", "Venus", "Earth", "Pluto"))
+
+        println("Vector capacity = " + planets2.capacity())
     }
 }
